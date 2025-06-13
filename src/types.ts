705b4cc9072ba5c459597646ad78cb4b0f7196f2
Emma Zuehlcke <git@emz.im>
2025-06-13 22:24:48 +0200

    Don't forget the license headers

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import { URLClassifierExceptionListEntry } from "./url-classifier-exception-list-types";
 
 // The RemoteSettings entry has a last_modified timestamp that is not exposed via the schema.
